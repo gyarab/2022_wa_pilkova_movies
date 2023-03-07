562242408a9355b6f898714904d56e7d4d49ed8d
dancapilkova <83206276+dancapilkova@users.noreply.github.com>
2023-03-07 09:18:02 +0100

    Add files via upload

--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-class Movie():
+class Director(models.Model):
+    name = models.CharField(max_length=200)
+    birth_year = models.IntegerField()
+
+class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     description = models.TextField()
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
