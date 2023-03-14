b20d84468ea6b9cc518e97f81b2752d75ba7e4b7
dancapilkova <83206276+dancapilkova@users.noreply.github.com>
2023-03-14 09:20:36 +0100

    Add files via upload

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import Movie
 from .models import Director
 
-admin.site.register(Movie)
-admin.site.register(Director)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year', 'director']
+    list_display_links = ['name']
+    search_fields = ['name']
+class DirAdmin(admin.ModelAdmin):
+    list_display = ['name', 'birth_year']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Director, DirAdmin)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Director(models.Model):
     name = models.CharField(max_length=200)
+    
     birth_year = models.IntegerField()
+    def _str_(self):
+        return f"{self.name} ({self.birth_year})"
 
 class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     description = models.TextField()
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
+
+    def _str_(self):
+       return f"{self.name} ({self.year}) {self.director}"
