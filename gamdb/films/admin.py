736dfb78951e149ec0d6c809005d648cabbca7b3
dancapilkova <daniela.pilkova@student.gyarab.cz>
2023-03-28 07:58:43 +0200

    genres

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 
-from .models import Movie
-from .models import Director
+from .models import Movie, Director, Genre
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'year', 'director']
+    list_display = ['id', 'name', 'year', 'director', 'genres_display']
     list_display_links = ['name']
     search_fields = ['name']
-class DirAdmin(admin.ModelAdmin):
-    list_display = ['name', 'birth_year']
-    list_display_links = ['name']
-    search_fields = ['name']
+    list_filter = ['genres', 'year']
 
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Director, DirAdmin)
+admin.site.register(Director)
+admin.site.register(Genre)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 
-class Director(models.Model):
+class Movie(models.Model):
     name = models.CharField(max_length=200)
-    
-    birth_year = models.IntegerField()
+    year = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    genres = models.ManyToManyField('Genre', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+
+    def genres_display(self):
+        return ", ".join([i.name for i in self.genres.all()])
+        # out = ""
+        # for i in self.genres.all():
+        #     out += f"{i.name}, "
+        # return out
+
+class Director(models.Model):
+    name = models.CharField(max_length=255)
+    birth_year = models.IntegerField(blank=True, null=True)
+
     def __str__(self):
         return f"{self.name} ({self.birth_year})"
 
-class Movie(models.Model):
-    name = models.CharField(max_length=200)
-    year = models.IntegerField()
-    description = models.TextField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-       return f"{self.name} ({self.year}) {self.director}"
+        return f"{self.name}"
